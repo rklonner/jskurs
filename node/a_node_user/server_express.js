490f6b9efca912e23e3b6a9afcd3be5d251c788f
rklonner <r.klonner@gmx.at>
2018-01-29 16:55:14 +0100

    tag4

--- node/a_node_user/server_express.js
+++ node/a_node_user/server_express.js
@@ -17,6 +17,7 @@ app.use(function(req, res, next) {
 // static files
 app.use(express.static('files'));
 
+app.use(bp.urlencoded({extended:true})); // POST Daten parsen
 
 app.get('/', function(req, res) {
   fs.readFile('vorlage.html', function(err, data) {
@@ -27,6 +28,23 @@ app.get('/', function(req, res) {
 
 
 app.post('/userverwaltung', function(req, res) {
-  res.writeHead(200, {'Content-Type':'text/html'});
-  res.end('OK');
+  fs.readFile('user.json', function(err, data) {
+    var userdaten = JSON.parse(data);
+    console.log(userdaten);
+    console.log(req.body.uname)
+    userdaten.user.push(req.body.uname);
+    fs.writeFile('user.json', JSON.stringify(userdaten), function() {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end('OK');
+    });
+  });
+});
+
+app.get('/userverwaltung', function(req, res) {
+  fs.readFile('user.json', function(err, data) {
+    var userdaten = JSON.parse(data);
+    //res.writeHead(200, {'Content-Type': 'JSON'});
+    //res.end(JSON.stringify(userdaten));
+    res.send(userdaten);  // express
+  });
 });
