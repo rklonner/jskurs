1dcacffd11b907c2748678782de83979b2805997
rklonner <r.klonner@gmx.at>
2018-01-30 12:35:48 +0100

    tag4 | input number bugfix chrome

---
+++ node/a_node_elefanten_restful_api/node/startme.js
@@ -0,0 +1,17 @@
+"use strict"
+
+var fs = require('fs');
+var cp = require('child_process');
+
+var serverFile = 'server_restful.js';
+
+// subprocess starten
+var server = cp.fork(serverFile);
+console.log('serverscript gestartet');
+
+fs.watchFile(serverFile, function() {
+  server.kill();
+  console.log("Serverscript beendet");
+  server = cp.fork(serverFile);
+  console.log('serverscript gestartet');
+});
